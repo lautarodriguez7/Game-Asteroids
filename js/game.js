ef721c972b7b97e8c934b8483b75f6a420cb89a0
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-15 16:00:28 -0300

    new variables

--- js/game.js
+++ js/game.js
@@ -1,8 +1,24 @@
 (function (window, undefined) {
     'use strict'; 
+window.addEventListener('load',init,false);
 
-var k = 10;
-var player = new Circle(40, 40, 5);
-var speed = 0;
+var KEY_LEFT = 37;
+var KEY_RIGHT = 39;
+var K1 = 10;
+var K2 = 1.1;
+var K3 = 0.5;
+
+var canvas = null, ctx = null;
+var lastPress = null;
+var pressing = [];
+var player1 = new Circle(40, 40, 5);
+var player2 = new Circle(40, 100, 5);
+var player3 = new Circle(40, 160, 5);
+var speed1 = 0,
+    speed2 = 0,
+    speed3 = 0;
+
+    
+
 
 }(window));
