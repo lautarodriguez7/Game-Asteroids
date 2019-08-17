415437b1a7eb8caaad518d8c4171fdd7b207ecf2
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-16 21:00:03 -0300

    new function "circle.prototype.stroke"

--- js/game.js
+++ js/game.js
@@ -131,7 +131,11 @@
         this.rotation = 0;
     }
 
-    
+    Circle.prototype.stroke=function(ctx){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
+        ctx.stroke();
+    }
 
     Circle.prototype.drawImage=function(ctx,img){
         if(img.width){
